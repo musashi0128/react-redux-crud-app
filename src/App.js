43342090c2160fcc8744e8f4dc18276580ec4bce
musashi0128 <k.miyamoto.0128@gmail.com>
2019-06-24 16:17:32 +0900

    push

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from 'react';
 // import React, {Component} from 'react';
-
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
-}
+import React from 'react';
 
 // class App extends Component {
-//     render() {
-//       return <div>Hello, world!</div>
-//     }
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("change")}} />
+//       </React.Fragment>
+//     )
+//   }
 // }
+
+const App = () =>  {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
 
 export default App;
